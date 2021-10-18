6a368bae5692bcb51998bba520cb91cb0af905d9
raythompsonwebdev <ray_thomp@hushmail.com>
2021-10-18 12:04:12 +0100

    code read example no3

---
+++ 3.js
@@ -0,0 +1,24 @@
+function funcOne(arr){
+
+    return arr.filter((element, index, arrayObj) => arrayObj.indexOf(element) == index );
+
+}
+// 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+console.log(funcOne([2,2,3,6,7,7,7,7,8,9])); //returns [2, 3, 6, 7, 8, 9]
