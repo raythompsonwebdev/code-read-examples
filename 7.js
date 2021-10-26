f3a036af8d3e544b414307df98c4908e5afd5c51
raythompsonwebdev <ray_thomp@hushmail.com>
2021-10-26 19:35:20 +0100

    code-read-example 7 & 8

---
+++ 7.js
@@ -0,0 +1,50 @@
+
+function funcOne(obj){
+
+    let arryOne = [];
+
+    for(let item in obj){
+
+        if(/[aeiou]/.test(obj[item])){
+
+            arryOne.push(obj[item])
+
+        }
+
+    }
+
+    return arryOne.sort();
+
+    
+
+}
+//
+//
+//
+//
+///
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+let obj = {
+    1:"paper",
+    2:"solid",
+    3:"cysts",
+    4:"flyby",
+    5:"pepper",
+    6:"water",
+    7:"glyph",
+    8:"candy"
+}
+console.log(funcOne(obj));
---
+++ 8.js
@@ -0,0 +1,27 @@
+
+function funcOne(...arg){
+
+    return arg.map(e => e ).sort();
+
+}
+//
+//
+//
+//
+///
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+console.log(funcOne(200, 300, 400, 1000, 800));
